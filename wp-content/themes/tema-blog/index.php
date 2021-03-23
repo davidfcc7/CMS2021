735bdf223bdaf0a30c3d6f82583ee08e218c03de
David Caicedo <davidfcc7@gmail.com>
2021-03-23 13:02:02 -0500

    creacion index.php

---
+++ wp-content/themes/tema-blog/index.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <!--Inicio nav-->
+    <div class="container-fluid-nav">
+        <nav class="navbar  sticky-top fixed-top navbar-expand-lg navbar-light container">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Home</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" aria-current="page" href="#">Blog</a>
+                    </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </div>
+    <!--Fin nav-->
+
+    <!--Inicio Informacion-->
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+              <div class="col-sm-6 col-md-6">
+                <h1>Información</h1>
+                <p class="text-ph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              </div>
+              <div class="col-sm">
+                <img class="image-info" src="./images/home-office-336377_1920.jpg" alt="imagen de informacion">
+              </div>
+            </div>
+          </div>
+    </div>
+    <!--Inicio Informacion-->
+
+    <!--Inicio Contenido Blog-->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="card col-12 col-sm-6 col-md-4 mb-3">
+                <img src="./images/home-office-336377_1920.jpg" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Etiqueta / Autor / Fecha / Categoria</small>
+                </div>
+            </div>
+            <div class="card col-12 col-sm-6 col-md-4 mb-3">
+                <img src="./images/home-office-336377_1920.jpg" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Etiqueta / Autor / Fecha / Categoria</small>
+                </div>
+            </div>
+            <div class="card col-12 col-sm-6 col-md-4 mb-3">
+                <img src="./images/home-office-336377_1920.jpg" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Etiqueta / Autor / Fecha / Categoria</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Fin Contenido Blog-->
+
+    <!--Inicio Footer-->
+    <div class="container-fluid bg-inverse py-3">
+        <div class="container text-center">
+            <p>Blog a la medida</p>
+        </div>
+    </div>
+    <!--Fin Footer-->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+    <script src="./js/bootstrap.min.js"></script>
+  </body>
+</html>
